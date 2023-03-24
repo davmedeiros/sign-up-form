45adfe7c1d23625bad154bd0420c4b5531d992b7
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-03-24 10:42:35 +0000

    Validate if passwords match

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 function main() {
-  return 0;
+  const submitButton = document.querySelector('#create-account-button');
+  submitButton.addEventListener('click', (e) => {
+    if (!validatePassword()) {
+      e.preventDefault();
+    }
+  });
+}
+
+function validatePassword() {
+  const password = document.querySelector('#password');
+  const confirmPassword = document.querySelector('#confirm-password');
+  console.log(password.value);
+  console.log(confirmPassword.value);
+  if (password.value !== confirmPassword.value) {
+    const errorMessage = document.createElement('p');
+    errorMessage.classList.add('error');
+    errorMessage.textContent = "Passwords do not match";
+    const formCard = document.querySelector('#form-card');
+    formCard.appendChild(errorMessage);
+    return false;
+  }
+  return true;
 }
 
 main();
